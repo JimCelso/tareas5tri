0404fb769cfc778b6bde8f40c5786e04ee322766
JimCelso <celsojms04@gmail.com>
2025-02-25 10:04:19 -0600

    ejercio3

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -4,5 +4,5 @@ let ingrediente1 = "Pizza con Piña";
 let precio = "24";
 
 //document.write(ingrediente1);
-document.getElementById('demo').innerHTML="Lista: " + ingrediente1;
+document.getElementById('demo').innerHTML="" + ingrediente1;
 document.getElementById('precio'). innerHTML="El precio es: " + precio;
