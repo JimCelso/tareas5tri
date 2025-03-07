99a35a1afae6336cb61a400b6d326ba4731c01cc
JimCelso <celsojms04@gmail.com>
2025-03-07 09:23:17 -0600

    Tarea 2

---
+++ ejercicio4(tarea2)/script.js
@@ -0,0 +1,11 @@
+let p1= "Para responder la pregunta anterior, primero debemos saber a que se conoce como una tecnología web, la cual se define como un conjunto de herramientas, lenguajes, protocolos y frameworks utilizados para desarrollar, implementar y gestionar aplicaciones y sitios web. Estas tecnologías permiten la creación de interfaces interactivas, el procesamiento de datos, la comunicación entre servidores y clientes, y la gestión de bases de datos, entre otras funciones. Se dividen en dos grandes categorías: tecnologías del lado del cliente (front-end) y tecnologías del lado del servidor (back-end).";
+
+let p2= "Una vez comprendido lo anterior, estas herramientas resultan esenciales para mi formación académica porque integran la teoría con la práctica, permitiéndome aplicar conceptos aprendidos en el aula a proyectos reales. El uso de estas tecnologías fomenta el desarrollo del pensamiento crítico y la resolución de problemas, habilidades fundamentales en cualquier carrera. Además, su dominio me prepara para los desafíos del mundo laboral, donde la innovación y la adaptabilidad son clave para el éxito profesional. En definitiva, la integración de estas herramientas en mi formación me capacita para ser un profesional competente y actualizado en un entorno digital en constante evolución.";
+
+document.getElementById('parr1').innerHTML=p1;
+
+document.getElementById('parr2').innerHTML=p2;
+
+console.log(p1.length);
+console.log(p2.length);
+
